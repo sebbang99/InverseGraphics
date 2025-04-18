8711c1f9c071756ea8edcf4612c5a98450a13166
seheecho <sehee138@sogang.ac.kr>
2025-04-18 19:23:13 +0900

    part1 task1 구현 완료

--- CSE6518_hw2/src/dataset/field_dataset_image.py
+++ CSE6518_hw2/src/dataset/field_dataset_image.py
@@ -4,13 +4,28 @@ from torch import Tensor
 
 from .field_dataset import FieldDataset
 
+import torch
+from PIL import Image
+import torchvision.transforms as T
+import torch.nn.functional as F
+
 
 class FieldDatasetImage(FieldDataset):
     def __init__(self, cfg: DictConfig) -> None:
         """Load the image in cfg.path into memory here."""
 
+        print(cfg)
+
         super().__init__(cfg)
-        raise NotImplementedError("This is your homework.")
+
+        image = Image.open(cfg.path).convert("RGB")
+
+        transform = T.ToTensor()  # [0, 1]로 만들기
+        self.image = transform(image)  # image를 자동으로 멤버 변수로 등록
+
+        self.height, self.width = self.image.shape[1], self.image.shape[2]
+        print(self.image.shape)  # shape (3, H, W)
+        # raise NotImplementedError("This is your homework.")
 
     def query(
         self,
@@ -24,6 +39,21 @@ class FieldDatasetImage(FieldDataset):
         parameter.
         """
 
+        image = self.image.unsqueeze(0)  # image shape (1, 3, H, W)
+        print(f"{image.shape=}")
+
+        grid = coordinates * 2 - 1  # grid shape (4, 2)
+        print(f"{grid.shape=}")
+
+        grid = grid.view(1, -1, 1, 2)  # grid shape (1, 4, 1, 2)
+        print(f"{grid.shape=}")
+
+        sampled = F.grid_sample(
+            image, grid, align_corners=True, mode="bilinear", padding_mode="border"
+        )
+        print(f"{sampled.shape=}")
+
+        return sampled.squeeze(0).squeeze(2).permute(1, 0)
         raise NotImplementedError("This is your homework.")
 
     @property
--- CSE6518_hw2/tests/test_field_dataset_image.py
+++ CSE6518_hw2/tests/test_field_dataset_image.py
@@ -32,7 +32,18 @@ def test_sampling():
         [0, 1, 0],
     ]
 
+    # debugging
+    print("here")
+    print(dataset.query(f32(coordinates)))
+
+    # 샘플링 후 결과 비교
+    # 같으면 true 반환, 다르면 false로 assert
     assert torch.allclose(
         dataset.query(f32(coordinates)),
         f32(expected),
     )
+
+    print("test completed.")
+
+
+test_sampling()
